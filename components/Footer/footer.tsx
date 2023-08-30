b42b6b472a2a2ef257205971a83d4226d2c1a9a7
SaiPranay2011 <chebium.pranay@aney.com>
2023-08-30 23:13:16 +0530

    completed project,contact,footer section

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -1,8 +1,61 @@
+import { useEffect, useState } from "react";
+import styles from "@/public/styles/components/footer.module.scss";
+import { socialMedia, github } from "@/public/config";
+import Icon from "../icons/icon";
+
 const Footer = () => {
+  const [githubInfo, setGitHubInfo] = useState({
+    stars: null,
+    forks: null,
+  });
+  
+  useEffect(() => {
+    fetch("https://api.github.com/repos/SaiPranay2011/Portfolio-V2")
+    .then(response => response.json())
+    .then(json => {
+      const {stargazers_count,forks_count} = json;
+      setGitHubInfo({
+        stars:stargazers_count,
+        forks:forks_count,
+      });
+    })
+    .catch(e => console.error(e))
+    
+  }, []);
+
   return (
-    <section id="footer">
-      <p style={{ marginTop: "1000px" }}>Hello</p>
-    </section>
+    <footer id="footer" className={styles.footerContainer}>
+      <div className={styles.footerSocialLinks}>
+        <ul>
+          {socialMedia &&
+            socialMedia.map(({ name, url }, i) => (
+              <li key={i}>
+                <a href={url}>
+                  <Icon name={name} />
+                </a>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className={styles.footerCredit}>
+        <a href={github}>
+          <div>Designed &amp; Built by Chebium Sai Pranay</div>
+
+          {githubInfo && (
+            <div className={styles.footerGithubStats}>
+              <span>
+                <Icon name="Star" />
+                <span>{githubInfo["stars"]}</span>
+              </span>
+              <span>
+                <Icon name="Fork" />
+                <span>{githubInfo["forks"]}</span>
+              </span>
+            </div>
+          )}
+        </a>
+      </div>
+    </footer>
   );
 };
 export default Footer;
--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   email: "chebiumsaipranay@gmail.com",
-
+  github:"https://github.com/SaiPranay2011",
   socialMedia: [
     {
       name: "GitHub",
@@ -60,12 +60,11 @@ module.exports = {
       location: "Dubai, UAE",
       range: "November 2022 - June 2023",
       url: "https://www.amal.com/en",
-      description:[
+      description: [
         "Built web pages and mobile application pages using react and react native.",
         "Worked on IOS and Android applications.",
-        "Worked with back end services like firebase and microservices."
+        "Worked with back end services like firebase and microservices.",
       ],
-        
     },
     {
       title: "Intern",
@@ -73,8 +72,8 @@ module.exports = {
       location: "Perak, Malaysia",
       range: "19th June, 2022 - 25th June, 2022",
       url: "",
-      description:[
-        "It was a immersion program where we were taught courses as well as buisiness management and projects were done individually and by group."
+      description: [
+        "It was a immersion program where we were taught courses as well as buisiness management and projects were done individually and by group.",
       ],
     },
     {
@@ -83,55 +82,91 @@ module.exports = {
       location: "Vijayawada, Andhra Pradesh",
       range: "June 2021 - July 2021",
       url: "",
-      description:["Industrial Training Internship on Android App Development using Java"],
-       
-    }
+      description: [
+        "Industrial Training Internship on Android App Development using Java",
+      ],
+    },
   ],
 
-  featuredProjects:[
+  featuredProjects: [
     {
-      title:"Portfolio-V1",
-      cover:"/assets/Projects/portfolioV1.png",
-      github:"https://github.com/SaiPranay2011/Portfolio-V1",
-      external:"",
-      tech:[
-        "React",
-        "CSS"
-      ],
-      description:"A Personal Portfolio Version-1 developed by using web technology ReactJS. Portfolio comprise of my personal information and my projects."
+      title: "Portfolio-V1",
+      cover: "/assets/Projects/portfolioV1.png",
+      github: "https://github.com/SaiPranay2011/Portfolio-V1",
+      external: "",
+      tech: ["React", "CSS"],
+      description:
+        "A Personal Portfolio Version-1 developed by using web technology ReactJS. Portfolio comprise of my personal information and my projects.",
     },
   ],
-  projects:[
+  projects: [
     {
-      title:"Amal web and mobile application",
-      external:"https://www.amal.com/en",
-      tech:[
-        "NextJS",
-        "ReactNative",
-        "Firebase",
-        "Jira",
-      ],
-      company:"Amal",
-      showInProjects: true,
-    },
-    {
-      title: "MerchWorld",
-      tech:[
-        "React",
-        "FastAPI"
-      ],
-      showInProjects:true,
-      description:"The website acts as a centralised platform for actors, musicians, YouTubers, and other artists to showcase and sell their items globally"
+      title: "Amal web and mobile application",
+      external: "https://www.amal.com/en",
+      tech: ["NextJS", "ReactNative", "Firebase", "Jira"],
+      company: "Amal",
+      showProject: true,
     },
     {
       title: "Meble",
-      tech:[
-        "Java",
-        "XML",
-        "Android Studio"
-      ],
-      showInProjects:false,
-      description:"An app designed using Java and Android Studio which enables users to select from a wide range of traditional and contemporary furniture to improve their aesthetic appearance of their commercial or private property."
-    }
+      github: "https://github.com/SaiPranay2011/Meble",
+      tech: ["Java", "XML", "Android Studio"],
+      showProject: true,
+      description:
+        "An app designed using Java and Android Studio which enables users to select from a wide range of traditional and contemporary furniture to improve their aesthetic appearance of their commercial or private property.",
+    },
+    {
+      title: "MerchWorld",
+      github: "https://github.com/SaiPranay2011/Merch-World",
+      tech: ["HTML", "CSS", "Javascript"],
+      showProject: true,
+      description:
+        "The website acts as a centralised platform for actors, musicians, YouTubers, and other artists to showcase and sell their items globally",
+    },
+    {
+      title: "Notes App",
+      github: "https://github.com/SaiPranay2011/notes-app",
+      tech: ["React","CSS"],
+      showProject: false,
+      description:
+        "I created a Notes website using react that allows users to write, edit, and store notes. This website has a JSON-based storage system, which ensures that the notes remain even after the page is reloaded",
+    },
+  ],
+  archiveProjects:[
+    {
+      title: "Amal web and mobile application",
+      external: "https://www.amal.com/en",
+      tech: ["NextJS", "ReactNative", "Firebase", "Jira"],
+      company: "Amal",
+      date:"2023",
+      showProject: true,
+    },
+    {
+      title: "Meble",
+      github: "https://github.com/SaiPranay2011/Meble",
+      tech: ["Java", "XML", "Android Studio"],
+      showProject: true,
+      date:"2021",
+      description:
+        "An app designed using Java and Android Studio which enables users to select from a wide range of traditional and contemporary furniture to improve their aesthetic appearance of their commercial or private property.",
+    },
+    {
+      title: "MerchWorld",
+      github: "https://github.com/SaiPranay2011/Merch-World",
+      tech: ["HTML", "CSS", "Javascript"],
+      showProject: true,
+      date:"2022",
+      description:
+        "The website acts as a centralised platform for actors, musicians, YouTubers, and other artists to showcase and sell their items globally",
+    },
+    {
+      title: "Notes App",
+      github: "https://github.com/SaiPranay2011/notes-app",
+      tech: ["React","CSS"],
+      showProject: false,
+      date:"2021",
+      description:
+        "I created a Notes website using react that allows users to write, edit, and store notes. This website has a JSON-based storage system, which ensures that the notes remain even after the page is reloaded",
+    },
   ]
 };
