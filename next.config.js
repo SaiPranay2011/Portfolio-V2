9f0640509b175cf3bdd4834182ec59d14fb4db9e
SaiPranay2011 <chebium.pranay@aney.com>
2023-09-08 17:48:33 +0530

    added basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
+    output:"export",
+    basePath:"https://saipranay2011.github.io/Portfolio-V2/"
 }
 
 module.exports = nextConfig
