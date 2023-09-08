afabe3f3e08d46c6aef7e9c3cb39a79b3c6ec169
SaiPranay2011 <chebium.pranay@aney.com>
2023-09-08 19:42:13 +0530

    minor change

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
-require("dotenv").config();
-const webpack = require("webpack");
+// require("dotenv").config();
+// const webpack = require("webpack");
 const nextConfig = {
   output: "export",
   basePath: "https://saipranay2011.github.io/Portfolio-V2/",
 };
 
-module.exports = {
-  nextConfig,
-  webpack: (config) => {
-    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
-    return config;
-  },
-};
+module.exports = nextConfig
+//   webpack: (config) => {
+//     config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+//     return config;
+//   },
+
